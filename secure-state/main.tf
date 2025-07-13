32cd323a4d07fcd043cce922b8d41c774e0c2af3
Visweshbonam <viswesh.bonam@apoorva.com>
2025-07-13 14:15:32 +0530

    securestate

--- secure-state/main.tf
+++ secure-state/main.tf
@@ -1,34 +1,34 @@
 resource "aws_instance" "JoinDevOps" {
-  ami = "ami-09c813fb71547fc4f"
-  instance_type = "t3.micro"
-  vpc_security_group_ids = [aws_security_group.allow_default.id]
+  ami                    = "ami-09c813fb71547fc4f"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow_default_new.id]
 
   tags = {
     Name = "JoinDevOps-1"
   }
 }
 
-resource "aws_security_group" "allow_default" {
-  name = "allow_default"
-  description = "sg for allow_default"
+resource "aws_security_group" "allow_default_new" {
+  name        = "allow_default_new"
+  description = "sg for allow_default_new"
   ingress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
 
   egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
 
 
-   tags = {
-    Name = "allow_default"
+  tags = {
+    Name = "allow_default_new"
   }
 }
